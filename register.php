2427b6f0e583831ceb828330ea8c70e334da6b5e
MMoreira01 <86256848+MMoreira01@users.noreply.github.com>
2021-12-23 16:12:40 +0000

    Valid inputs on register form stay
    Invalid inputs disappear

--- register.php
+++ register.php
@@ -139,12 +139,15 @@
 	if (!empty($existingRecords)) {
 
 		if ($existingRecords['username'] && $existingRecords['email']) {
+			unset($_POST);
 			//both the username and the email already exist in the database
 			echo "Both username and email already exist in our records.";
 		} elseif ($existingRecords['username']) {
+			unset($_POST['username']);
 			//only the username exists (you can erase the written username so that it does not show up in the filled form, but it seams better to keep it so that the user knows what was the input)
 			echo "This username is already taken. Please choose another one.";
 		} else {
+			unset($_POST['email']);
 			//only the email exists (you can erase the written email so that it does not show up in the filled form, but it seams better to keep it so that the user knows what was the input)
 			echo "This email is already taken. Please choose another one.";
 		}
@@ -179,7 +182,7 @@
  			<img class="loginLogo" src="../TH/img/logo.png" />
  			<form class="loginForm" action="" method="POST">
  				<input class="loginInput" type="text" id="username" name="username" placeholder="Username" value="<?php
-					if (!empty($errors) && !isset($_errors['username'][0])) { #this is done to keep the value inputted by the user if this field is valid but others are not
+					if (!empty($errors) && isset($errors['username'][0])) { #this is done to keep the value inputted by the user if this field is valid but others are not
 							echo $_POST['username'];
 						}
 						?>"><br>
@@ -191,8 +194,9 @@
 					?>
  				<input class="loginInput" type="email" id="email" name="email" placeholder="E-mail" value="<?php
 
-				if (!empty($errors) && !isset($_errors['email'][0])) {
+				if (!empty($errors && isset($errors['email'][0])) ) {
 						echo $_POST['email'];
+						
 					}
 					?>"><br>
  				<?php
