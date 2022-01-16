932f308a9ba19d47898758e41bbdbc079d1d627b
Cascavel <simao.ramaral@gmail.com>
2022-01-16 02:48:16 +0000

    updated front-end of close_account.php

--- close_account.php
+++ close_account.php
@@ -46,20 +46,27 @@ if (isset($_POST['closeAccount'])) {
 <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
 </head>
 <body style="background-color:#282a36">
-<div class="container">
+<div class="container-fluid">
     <div class="row justify-content-md-center">
-    <div class="col-sm-6">
+    <div class="col-md-6">
     <div style="background-color:#44475a" class="card-header">
     <h4 class="mt-2 text-white">Close Account</h4>
     </div>
-    <div class="card-body p-4">
+    <div class="card-body text-center">
+        <div class="row">
+        <img class="img-fluid rounded" src="https://media.giphy.com/media/IzdL5AH7teZSOPnqrw/giphy.gif">
+        </div>
+        <div class="row mt-4">
         <span class="text-danger h4">Warning!</span>
         <p class="text-white">Are you sure you want to close your account, <?php echo $userLoggedIn;?>?</p>
+        </div>  
     </div>
-<form class="text-center" action="close_account.php" method="POST">
-    <input class="btn btn-danger btn-sm" type="submit" name="closeAccount" id="closeAccount" value="Close it!">
-    <input class="btn btn-primary btn-sm" type="submit" name="cancel" value="No, take me back!">
+    <div class="row align-items-center">
+    <form class="text-center" action="close_account.php" method="POST">
+            <input class="btn btn-danger btn-sm" type="submit" name="closeAccount" id="closeAccount" value="Close it!">
+            <input class="btn btn-primary btn-sm" type="submit" name="cancel" value="No, take me back!">
 </form>
+    </div>
     </div>
     </div> 
     
